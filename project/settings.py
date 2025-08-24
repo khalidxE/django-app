544de484f9a706e6d81934f630ca45f7e065f252
khalidxE <khalid.development.101@gmail.com>
2025-08-24 21:47:06 +0200

    edit settings

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-2ryzvj936_=w_6q4=p7e)j_08^0^k9#4u__jvzc1tj+_y=m=$d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-app-1-ofgc.onrender.com' ,
+	]
 
 
 # Application definition
